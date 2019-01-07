8e48a042d9c15ecae954ac1370330d7a790e04d6
Martin <mart73p5@edu.easj.dk>
2019-01-07 15:24:35 +0100

    Initial commit

---
+++ src/MartinSlugFX.java
@@ -0,0 +1,14 @@
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.settings.GameSettings;
+
+public class MartinSlugFX extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+
+        gameSettings.setTitle("Martin Slug");
+        gameSettings.setWidth(800);
+        gameSettings.setHeight(600);
+
+    }
+}
